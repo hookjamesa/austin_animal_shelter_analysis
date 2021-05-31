1f42743a33209fc352abbbb7572b01cfbb5eb6de
Elijah Belizor <e.belizor@utexas.edu>
2021-05-31 17:23:47 -0500

    creating ml_df

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE animal_type AS
+SELECT animal_id_outcome AS animal_id, breed, color, animal_type
+FROM raw_input_outcome;
+
+SELECT * FROM raw_input_outcome;
+
+SELECT * FROM animal_type;
